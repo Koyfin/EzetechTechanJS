8e082dfafeb8f2e4e65d6d8293ef168eb6670296
Vlad <vlad@MacBook-Air-Vlad-2.local>
2016-05-09 15:47:32 +0300

    test commit

--- src/techan.js
+++ src/techan.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var _d3;
-
+// test commit
 // If running in browser (window !undefined), assume d3 available
 if('undefined' != typeof window) _d3 = window.d3;
 else if('object' == typeof module) _d3 = require('d3'); // else we're in the only other supported mode: v8/node
